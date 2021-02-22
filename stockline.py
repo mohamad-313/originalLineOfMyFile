a99183f403457d1200f5cb5bfa8ebbb650a9a5f0
I'm A Programmers(Human In virgool) <76949372+mohamad-313@users.noreply.github.com>
2021-02-22 10:24:37 +0330

    Add files via upload

---
+++ stockline.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+def emptryLine(nameFile):
+    f = open(nameFile,"r")
+    lines = f.readlines()
+    emptryLineCount = 0
+
+    for i in lines:
+        emptryLineCount += 1 if i.strip() == "" else 0
+
+    print(f"Stock line number is {len(lines) - emptryLineCount}")
+
+if len(argv) == 2:
+    emptryLine(argv[1])
+else:
+    emptryLine(input("Name of file:\n"))
